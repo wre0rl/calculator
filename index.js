3f0be063c49d461ccd3e0a378b45300f77c8c152
wre0rl <wre0rl@gmail.com>
2023-05-20 16:45:35 +0700

    Created operate function and user input variable that calls one of the arithmetic functions

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+const a = 3, 
+      b = 3,
+      operator = 'multiply';
+
+function operate(a, op, b) {
+  calculate = {
+    'add': add(a, b),
+    'subtract': subtract(a, b),
+    'multiply': multiply(a, b),
+    'divide': divide(a, b)
+  };
+
+  return calculate[op];
+}
+
+console.log(operate(a, operator, b));

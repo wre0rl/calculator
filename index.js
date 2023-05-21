85202afb5064ae4e1219e55e1a738a394a4d5f47
wre0rl <wre0rl@gmail.com>
2023-05-21 20:17:52 +0700

    Refactor the if else statements and some functions for better readability

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+// Calculation functions
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 function operate(a, op, b) {
   a = parseFloat(a);
@@ -28,29 +18,27 @@ function operate(a, op, b) {
   showOperate(operators[op]);
 }
 
-function showOperate(res) {
-  clearVariable();
-  display.innerText = res;
-  a = res;
+//console.log(multiply(0.2, 3));
+
+function showOperate(result) {
+  clearVariables();
+  display.innerText = result;
+  a = result.toString();
 }
 
 function showDisplay(innerText) {
   display.innerText = `${a} ${operator} ${b}`;
 }
 
-function clearVariable() {
-  a = '';
-  b = '';
-  operator = '';
+function clearVariables() {
+  [a, b, operator] = ['0', '', ''];
 }
 
 function clearDisplay() {
-  display.innerText = '';
+  display.innerText = '0';
 }
 
-let a = '';
-let b = '';
-let operator = '';
+let [a, b, operator] = ['0', '', ''];
 
 // Event Listener
 const buttons = document.querySelectorAll('.button');
@@ -59,37 +47,48 @@ const operatorList = ['+', '-', '*', '/'];
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    let innerText = e.target.innerText;
+    const innerText = e.target.innerText;
+    const isOperator = operatorList.includes(innerText);
+    const isDecimal = display.innerText.includes('.');
 
     // Store operator
-    if (operatorList.includes(innerText)) {
+    if (isOperator) {
       if (a && b && operator) { // When there's a, operator, b and user clicked next operator it'll calculate
         operate(a, operator, b);
-        let previousOperator = innerText;
-        operator = previousOperator;
+        operator = innerText;
       } else {
         operator = innerText;
       }
     }
 
     // Store a & b
-    if (!operatorList.includes(innerText)) {
-      if (operator) {
-        b += innerText;
-      } else {
+    if (!isOperator) {
+      if (!operator) {
+        if (a.charAt(0) === '0') {
+          if (innerText !== '.' && !isDecimal) {
+            a = '';
+          }
+        }
         a += innerText;
+      } else {
+        b += innerText;
+        // TODO: Add 0 if user input . after operator e.g 0.2 + .2 ???
       }
     }
 
+    // Clear display & variables
     if (innerText === 'AC') {
       clearDisplay();
-      clearVariable();
-    } else if (innerText === '=') {
+      clearVariables();
+    }
+    
+    // Calculate
+    if (innerText === '=') {
       if (a && b && operator) {
         operate(a, operator, b);
       }
-    } else {
-      showDisplay();
     }
+    
+    showDisplay();
   });
 });

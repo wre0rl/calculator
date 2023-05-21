799fabe72e5e01335510510af31b31fc95ba4a47
wre0rl <wre0rl@gmail.com>
2023-05-21 17:02:41 +0700

    Added show display, clear display, clear variable, show operate and refactor some code

--- index.js
+++ index.js
@@ -14,76 +14,84 @@ function divide(a, b) {
   return a / b;
 }
 
-let a = '', 
-    b = '',
-    operator = '';
-
 function operate(a, op, b) {
-  calculate = {
+  a = parseFloat(a);
+  b = parseFloat(b);
+
+  const operators = {
     '+': add(a, b),
     '-': subtract(a, b),
     '*': multiply(a, b),
     '/': divide(a, b)
   };
 
-  return calculate[op];
+  showOperate(operators[op]);
 }
-//console.log(operate(a, operator, b));
 
+function showOperate(res) {
+  clearVariable();
+  display.innerText = res;
+  a = res;
+}
+
+function showDisplay(innerText) {
+  display.innerText = a + operator + b;
+}
+
+function clearVariable() {
+  a = '';
+  b = '';
+  operator = '';
+}
+
+function clearDisplay() {
+  display.innerText = '';
+}
+
+let a = '';
+let b = '';
+let operator = '';
 
 // Event Listener
 const buttons = document.querySelectorAll('.button');
 const display = document.querySelector('.display');
 const operatorList = ['+', '-', '*', '/'];
-const utilityList = ['=', 'AC'];
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    // Store the variable if operator button is clicked
-    if (a) {
-      if (operatorList.includes(e.target.innerText)) {
-        operator = e.target.innerText;
-        console.log(operator);
-        display.innerText += operator;
+    let innerText = e.target.innerText;
+
+    // Store operator
+    if (operatorList.includes(innerText)) {
+      let previousOperator = operator;
+      console.log(operator);
+      if (a && b && operator) { // When there's a, operator, b and user clicked next operator it'll calculate
+        operate(a, operator, b);
+        display.innerText += previousOperator;
+        operator = previousOperator;
+      } else {
+        operator = innerText;
       }
     }
 
-    // Store the variable if num button is clicked
-    if (operatorList.includes(e.target.innerText) === false && utilityList.includes(e.target.innerText) === false) {
-      display.innerText += e.target.innerText;
-      if (!operator) {
-        a += e.target.innerText;
-        console.log(a)
+    // Store a & b
+    if (!operatorList.includes(innerText)) {
+      if (operator) {
+        b += innerText;
       } else {
-        b += e.target.innerText;
-        console.log(b)
+        a += innerText;
       }
     }
 
-    
-    if (utilityList.includes(e.target.innerText)) {
-      if (e.target.innerText === '=') {
-        if (a && b && operator) {
-          display.innerText += e.target.innerText;
-          const calc = operate(parseFloat(a), operator, parseFloat(b));
-          display.innerText = calc;
-          a = calc; // Store the operate result for next calculation
-          // Reset the variable
-          b = '';
-          operator = '';
-        }
-      } else {
-        display.innerText = '';
-        a = '';
-        b = '';
-        operator = '';
+    if (innerText === 'AC') {
+      clearDisplay();
+      clearVariable();
+    } else if (innerText === '=') {
+      if (a && b && operator) {
+        operate(a, operator, b);
       }
-      
+    } else {
+      showDisplay();
     }
-    
-    // TODO
-    // Do this 12 + 7 - 5 * 3 = 42
-    // fix display (change into array for better styling support?)
-    // REfactor?
   });
 });
